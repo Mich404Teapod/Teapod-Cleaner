ef14c89245defe7b5536f9bebe43cb105a4b3388
Mich404Teapod <101505439+Mich404Teapod@users.noreply.github.com>
2022-03-28 16:55:08 +0100

    Create Interface.swift

---
+++ Cleaner/Mac-osConfig/Interface.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum InterfaceStyle : String {
+    case Dark, Light
+    
+    init() {
+        let type = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") ?? "Light"
+        self = InterfaceStyle(rawValue: type)!
+    }
+}
