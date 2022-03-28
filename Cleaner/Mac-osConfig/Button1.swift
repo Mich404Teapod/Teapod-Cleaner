2ff2f5964a304eaf96707cb7cd605fc92d6a7591
Mich404Teapod <101505439+Mich404Teapod@users.noreply.github.com>
2022-03-28 16:45:54 +0100

    Update Button1.swift

--- Cleaner/Mac-osConfig/Button1.swift
+++ Cleaner/Mac-osConfig/Button1.swift
@@ -1,16 +1,15 @@
 import Cocoa
-import LetsMove
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    //Close app when closing window
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true;
+class GrayButton: NSButton {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        if(InterfaceStyle().rawValue == "Dark"){
+            self.layer?.backgroundColor = CGColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3)
+        } else {
+            self.layer?.backgroundColor = CGColor.init(red: 0.3, green: 0.3, blue: 0.3, alpha: 0.3)
+        }
+        self.layer?.cornerRadius = 15.0
     }
-    
-    func applicationWillFinishLaunching(_ notification: Notification) {
-        PFMoveToApplicationsFolderIfNecessary()
-    }
-    
 }
